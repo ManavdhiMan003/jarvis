f4823fd33ef9c1ef1b5bc6056a01db1824912d63
manavdhiman <65016580+ManavdhiMan003@users.noreply.github.com>
2021-03-30 11:47:22 +0530

    add feature of opening links like youtube google

--- jarvis.py
+++ jarvis.py
@@ -8,6 +8,9 @@ Created on Mon Mar 29 10:35:56 2021
 import speech_recognition as sr
 import pyttsx3
 from scripts import * 
+import datetime
+import wikipedia
+import webbrowser
 r = sr.Recognizer()
 
 
@@ -20,10 +23,11 @@ def speak(data):
 # speak('How can i help you')
 while 1:
     cmd = get_command() 
+    cmd =  cmd.lower()
     if cmd=='':
         speak("sorry I didn't get it please repeat")
     else:
-        if 'send a mail' in cmd or 'send email' in cmd:
+        if 'send a mail' in cmd or 'send email' in cmd or 'mail' in cmd or 'email' in cmd:
             try:
                 speak("what is the message sir?")
                 message = get_command()
@@ -34,6 +38,22 @@ while 1:
             except Exception as e:
                 print(e)
                 speak("sorry sir i am unable to send email")
+        elif 'wikipedia' in cmd:
+            speak('Searching Wikipedia...')
+            cmd = cmd.replace('wikipedia',"")
+            result = wikipedia.summary(cmd,sentences=1)
+            speak("According to wikipedia")
+            print(result)
+            speak(result)
+        elif 'youtube' in cmd:
+            speak('opening youtube')
+            webbrowser.open('youtube.com')    
+        elif 'google' in cmd:
+            speak('opening google')
+            webbrowser.open('google.com')    
+        elif 'time' in cmd:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S") 
+            speak(f'Time is {strTime}')
         elif 'stop' in cmd or 'exit' in cmd:
             speak('Good Bye sir')
             break
